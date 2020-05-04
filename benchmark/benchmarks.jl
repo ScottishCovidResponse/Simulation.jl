3f8f73c22ee08b0d8796fc0a3a44e5762f2ac6c0
Bella Wu <bella.wu@invenialabs.co.uk>
2020-05-04 13:38:32 +0100

    add more examples for benchmarking

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,21 +1,35 @@
 # sort out dependencies
 using Pkg
 tempdir = mktempdir()
+path_to_repo = joinpath(@__DIR__, "..")
 Pkg.activate(tempdir)
-Pkg.develop(PackageSpec(path=joinpath(@__DIR__, "..")))
+Pkg.develop(PackageSpec(path=path_to_repo))
 Pkg.add(["BenchmarkTools", "PkgBenchmark"])  # for benchmarking
-Pkg.add(["Unitful", "Plots","PlotlyJS", "ORCA"])  # for running experiments
+Pkg.add(["StatsBase", "Unitful", "Plots", "PlotlyJS", "ORCA"])  # for running experiments. NOTE: keep plotting pkgs otherwise some code in examples can't complie (e.g. @layout)
 Pkg.resolve()
 
 using BenchmarkTools
 
 const SUITE = BenchmarkGroup()
-const PATH_TO_EXAMPLES = "../examples/Epidemiology/"
+const PATH_TO_REPO = "../"
+const PATH_TO_EXAMPLES = "examples/Epidemiology/" # from repo
 
-for file in readdir(joinpath(@__DIR__, PATH_TO_EXAMPLES))
-    # temporarily restrict to 2 files only as a prototype
-    if file in ["Benchmarking.jl", "Small_SIR.jl"]
+function run_example(file)
+    include(file)
+end
+
+# set to repo path as some examples use the relative path from repo to load the data
+cd(joinpath(@__DIR__, PATH_TO_REPO))
+for file in readdir(joinpath(@__DIR__, PATH_TO_REPO, PATH_TO_EXAMPLES))
+    # temporarily restrict to a few files only as a prototype
+    if file in [
+        # "Benchmarking.jl",
+        # "Small_SIR.jl",
+        # "SEI2HRD_example.jl",
+        "UK_SIR.jl"
+        ]
         SUITE[file[1:end - length(".jl")]] =
-            @benchmarkable include(joinpath(PATH_TO_EXAMPLES, $(file)))
+            @benchmarkable $(run_example)(joinpath(@__DIR__, PATH_TO_REPO, PATH_TO_EXAMPLES, $(file)))
     end
 end
+cd(@__DIR__)
