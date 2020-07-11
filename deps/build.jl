135f2e7c7d9051eaa8a929ebb460d404f4bf9ae8
Alex Robson <alex.robson@invenialabs.co.uk>
2020-07-11 18:17:01 +0100

    dev -> add

--- deps/build.jl
+++ deps/build.jl
@@ -7,5 +7,5 @@ using Pkg
 
 
 println("Adding SimulationData...")
-Pkg.develop(PackageSpec(url="https://github.com/ScottishCovidResponse/SimulationData.jl"))
+Pkg.add(PackageSpec(url="https://github.com/ScottishCovidResponse/SimulationData.jl"))
 Pkg.build("SimulationData")
