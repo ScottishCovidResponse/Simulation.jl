43d0f5e2b617ec8c803c92954cbc5884d5c4388d
Sean Lovett <sean.lovett@invenialabs.co.uk>
2020-07-17 10:40:20 +0100

    Only load ECMWF interface if asked for

--- src/ClimatePref/ClimatePref.jl
+++ src/ClimatePref/ClimatePref.jl
@@ -1,13 +1,14 @@
 using Requires
 function __init__()
-    if !env_bool("TRAVIS")
+    # Only try loading this when we're not on travis, and we've specified SIMULATION_ECMWF
+    if !env_bool("TRAVIS") && env_bool("SIMULATION_ECMWF")
         @require PyCall="438e738f-606a-5dbb-bf0a-cddfbfd45ab0" begin
             println("Creating ECMWF interface ...")
             include("ERA_interim_tools.jl")
             export retrieve_era_interim
             include("ECMWF_tools.jl")
             export retrieve_ECMWF
-    end
+        end
     end
 end
 
